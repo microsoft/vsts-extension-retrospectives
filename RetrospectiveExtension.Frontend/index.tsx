23c9cbbc7e8882cb3a5c3721030636ea9aaa7f17
polatengin <polatengin@outlook.com>
2020-05-28 12:00:38 +0300

    unused imports and variables removed

--- RetrospectiveExtension.Frontend/index.tsx
+++ RetrospectiveExtension.Frontend/index.tsx
@@ -6,17 +6,10 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './css/main.scss';
 
-import FeedbackBoard, { FeedbackBoardProps } from './components/feedbackBoard';
 import FeedbackBoardContainer, { FeedbackBoardContainerProps } from './components/feedbackBoardContainer';
 
 import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
 initializeIcons();
-
-import { WorkflowPhase } from './interfaces/workItem';
-import * as StubData from './stubData';
-
-import { initializeRetrospectiveWorkItemType } from './config/init';
-import { ExceptionCode } from './interfaces/retrospectiveState';
 
 import { appInsightsClient, TelemetryEvents } from './utilities/appInsightsClient'
 appInsightsClient.trackEvent(TelemetryEvents.ExtensionLaunched);
