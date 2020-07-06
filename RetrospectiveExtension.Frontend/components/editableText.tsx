0bf2bd36c3f612ff1b3753487cd54a3c0ef93c95
polatengin <polatengin@outlook.com>
2020-07-06 12:18:45 +0300

    added type to onclick event handler parameter

--- RetrospectiveExtension.Frontend/components/editableText.tsx
+++ RetrospectiveExtension.Frontend/components/editableText.tsx
@@ -148,7 +148,7 @@ export default class EditableText extends React.Component<EditableTextProps, Edi
             maxLength={this.props.maxLength}
             resizable={false}
             onKeyDown={this.handleKeyPress}
-            onClick={(e) => {
+            onClick={(e: MouseEvent) => {
               e.stopPropagation();
             }}
           />
