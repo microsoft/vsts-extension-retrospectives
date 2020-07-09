27f928ccb2d1cb90c1f78fe4d1a48e6ae9283816
polatengin <polatengin@outlook.com>
2020-07-09 12:24:52 +0300

    code styling fixed

--- RetrospectiveExtension.Frontend/components/noFeedbackBoardsView.tsx
+++ RetrospectiveExtension.Frontend/components/noFeedbackBoardsView.tsx
@@ -3,26 +3,26 @@ import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
 const NoWorkSvg = require('../images/zerodata-no-work-scheduled.svg');
 
 export interface NoFeedbackBoardsViewProps {
-    onCreateBoardClick: () => void
+  onCreateBoardClick: () => void
 }
 
 export default class NoFeedbackBoardsView extends React.Component<NoFeedbackBoardsViewProps> {
-    constructor(props: NoFeedbackBoardsViewProps) {
-        super(props);
-      }
+  constructor(props: NoFeedbackBoardsViewProps) {
+    super(props);
+  }
 
-      public render() {
-        return (
-            <div className="no-boards-container">
-                <img src={NoWorkSvg} alt="No Retrospectives found, create your first one to get started." />
-                <div className="no-boards-text">Get started with your first Retrospective</div>
-                <div className="no-boards-sub-text">Create a new board to start collecting feedback and create new work items.</div>
-                <DefaultButton
-                    primary={true}
-                    text="Create Board"
-                    onClick={this.props.onCreateBoardClick}
-                    className="create-new-board-button" />
-            </div>
-        )
-      }
+  public render() {
+    return (
+      <div className="no-boards-container">
+        <img src={NoWorkSvg} alt="No Retrospectives found, create your first one to get started." />
+        <div className="no-boards-text">Get started with your first Retrospective</div>
+        <div className="no-boards-sub-text">Create a new board to start collecting feedback and create new work items.</div>
+        <DefaultButton
+          primary={true}
+          text="Create Board"
+          onClick={this.props.onCreateBoardClick}
+          className="create-new-board-button" />
+      </div>
+    )
+  }
 }
