dfed27630e011d465384683139a042279588c90a
polatengin <polatengin@outlook.com>
2020-07-17 18:15:04 +0300

    svg dependency removed

--- RetrospectiveExtension.Frontend/components/noFeedbackBoardsView.tsx
+++ RetrospectiveExtension.Frontend/components/noFeedbackBoardsView.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
-const NoWorkSvg = require('../images/zerodata-no-work-scheduled.svg');
 
 export interface NoFeedbackBoardsViewProps {
   onCreateBoardClick: () => void
@@ -14,7 +13,6 @@ export default class NoFeedbackBoardsView extends React.Component<NoFeedbackBoar
   public render() {
     return (
       <div className="no-boards-container">
-        <img src={NoWorkSvg} alt="No Retrospectives found, create your first one to get started." />
         <div className="no-boards-text">Get started with your first Retrospective</div>
         <div className="no-boards-sub-text">Create a new board to start collecting feedback and create new work items.</div>
         <DefaultButton
