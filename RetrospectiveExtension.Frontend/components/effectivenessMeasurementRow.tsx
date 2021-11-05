bf90f2332aaf2722942038eb6e00648973f146ab
polatengin <enpolat@microsoft.com>
2021-11-04 18:10:21 -0700

    creating initial effectiveness measurement row react component

---
+++ RetrospectiveExtension.Frontend/components/effectivenessMeasurementRow.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+export interface EffectivenessMeasurementRowProps {
+}
+
+export interface EffectivenessMeasurementRowState {
+}
+
+export default class EffectivenessMeasurementRow extends React.Component<EffectivenessMeasurementRowProps, EffectivenessMeasurementRowState> {
+  constructor(props: EffectivenessMeasurementRowProps) {
+    super(props);
+  }
+  public render() {
+    return (
+    );
+  }
+}
