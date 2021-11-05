dfebecade89a8592487c1de83620f378eb0d0f16
polatengin <enpolat@microsoft.com>
2021-11-04 18:13:51 -0700

    calculating state fields based on current and prop values

--- RetrospectiveExtension.Frontend/components/effectivenessMeasurementRow.tsx
+++ RetrospectiveExtension.Frontend/components/effectivenessMeasurementRow.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
 
+import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
+import { getUserIdentity } from '../utilities/userIdentityHelper';
+
 export interface EffectivenessMeasurementRowProps {
   title: string;
   questionId?: string;
@@ -16,7 +19,20 @@ export interface EffectivenessMeasurementRowState {
 export default class EffectivenessMeasurementRow extends React.Component<EffectivenessMeasurementRowProps, EffectivenessMeasurementRowState> {
   constructor(props: EffectivenessMeasurementRowProps) {
     super(props);
+    const currentUserId = getUserIdentity().id;
+    const votes = this.props.votes || {};
+    const vote = votes[currentUserId] || [];
+    const currentVote = vote.filter(vote => vote.questionId === this.props.questionId || "");
+    this.state = {
+      selected: currentVote.length > 0 ? currentVote[0].selection : 0,
+    };
   }
+
+  updateSelected = (selected: number) => {
+    this.setState({ selected });
+    this.props.onSelectedChange(selected);
+  }
+
   public render() {
     return (
     );
