08e84ec23d6bd6dd3b2e1c952ead2513a694b086
Narmatha Balasundaram <narmathabala@gmail.com>
2022-02-10 15:17:19 -0500

    Backend deployment changes (#45)
    * [1418] Create dev backend IaC scripts and doc  (#6)
    
    * Updates to Backend to support developer backend deployment
    * Updates to Frontend to remove prod specific config
    * Integrate build and deploy .NET project into setup script
    * Add backend deployment instructions
    
    Co-authored-by: Ari Goldberg <argoldbe@microsoft.com>
    Co-authored-by: Mohamed Hassanin <mhassanin@microsoft.com>
    
    * Improve backend application insights and logging reporting (#32)
    
    - Improve backend application insights and logging reporting (#32)
    Co-authored-by: Phong Cao <thcao@microsoft.com>
    
    * Commenting out npm run test

--- RetrospectiveExtension.Backend/Configuration/ReflectKeyVaultSecretManager.cs
+++ RetrospectiveExtension.Backend/Configuration/ReflectKeyVaultSecretManager.cs
@@ -3,16 +3,16 @@ using Microsoft.Extensions.Configuration.AzureKeyVault;
 
 namespace CollaborationStateService.Configuration
 {
-  /// <summary>
-  /// Extends <see cref="DefaultKeyVaultSecretManager"/> to only load enabled secret items
-  /// and replaces '--' with ':" in the names.
-  /// </summary>
-  public class ReflectKeyVaultSecretManager : DefaultKeyVaultSecretManager
-  {
-    /// <inheritdoc />
-    public override bool Load(SecretItem secret)
+    /// <summary>
+    /// Extends <see cref="DefaultKeyVaultSecretManager"/> to only load enabled secret items
+    /// and replaces '--' with ':" in the names.
+    /// </summary>
+    public class ReflectKeyVaultSecretManager : DefaultKeyVaultSecretManager
     {
-      return secret.Attributes.Enabled ?? false;
+        /// <inheritdoc />
+        public override bool Load(SecretItem secret)
+        {
+            return secret.Attributes.Enabled ?? false;
+        }
     }
-  }
 }
--- RetrospectiveExtension.Backend/Program.cs
+++ RetrospectiveExtension.Backend/Program.cs
@@ -3,13 +3,13 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace CollaborationStateService
 {
-  public class Program
-  {
-    public static void Main(string[] args)
+    public class Program
     {
-      BuildWebHost(args).Run();
-    }
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
 
-    public static IWebHost BuildWebHost(string[] args) => WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build();
-  }
+        public static IWebHost BuildWebHost(string[] args) => WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build();
+    }
 }
