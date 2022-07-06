c143b5c8b9f06c23776524ed6452aa6224c76306
Engin Polat <enpolat@microsoft.com>
2022-07-06 22:37:29 +0000

    removing unneccesary logging to console

--- RetrospectiveExtension.Frontend/dal/itemDataService.tsx
+++ RetrospectiveExtension.Frontend/dal/itemDataService.tsx
@@ -205,7 +205,6 @@ class ItemDataService {
     const feedbackItem: IFeedbackItemDocument = await this.getFeedbackItem(boardId, feedbackItemId);
 
     if (!feedbackItem) {
-      console.log(`Cannot increment the timer seconds for a non-existent feedback item. Board: ${boardId}, Item: ${feedbackItemId}`);
       return undefined;
     }
 
