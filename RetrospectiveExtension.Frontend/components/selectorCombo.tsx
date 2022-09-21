fb82d9e2a355f178e8b2651719aff4ddccaa3a47
Hannah Kennedy <7254163+mindlessroman@users.noreply.github.com>
2022-09-21 18:41:22 -0400

    Fixes for the frontend CI errors.

--- RetrospectiveExtension.Frontend/components/selectorCombo.tsx
+++ RetrospectiveExtension.Frontend/components/selectorCombo.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
 import { FocusTrapCallout, DirectionalHint } from 'office-ui-fabric-react/lib/Callout';
-import { Dialog } from 'office-ui-fabric-react/lib/Dialog';
 import { List } from 'office-ui-fabric-react/lib/List';
 import { Shimmer } from 'office-ui-fabric-react/lib/Shimmer';
 import { TextField } from 'office-ui-fabric-react/lib/TextField';
