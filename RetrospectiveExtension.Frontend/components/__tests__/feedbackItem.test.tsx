876f3f5b66dcffc06d58847fd927433b89535f6b
Hannah Kennedy <7254163+mindlessroman@users.noreply.github.com>
2022-09-22 10:58:48 -0400

    Update the test for feedbackitem.tsx

--- RetrospectiveExtension.Frontend/components/__tests__/feedbackItem.test.tsx
+++ RetrospectiveExtension.Frontend/components/__tests__/feedbackItem.test.tsx
@@ -60,7 +60,7 @@ describe('Feedback Item', () => {
     // Expect basic values of the Feedback Item to be propagated in multiple areas of the rendered component.
     expect(component.findWhere((child) =>
       child.prop("className") === "anonymous-created-date").text()).
-      toEqual(moment(testFeedbackItem.createdDate).format('MMMM D, YYYY, h:mm A'));
+      toEqual(moment(testFeedbackItem.createdDate).format('MMMM D, YYYY [at] h:mm A'));
 
     expect(component.findWhere((child) =>
       child.prop("className") === "card-id").text()).
