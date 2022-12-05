c15f26f73a48fe36d201cf1ecc26df44b5342c15
Engin Polat <enpolat@microsoft.com>
2022-12-05 19:06:59 +0000

    simplifying getIdValue function

--- RetrospectiveExtension.Frontend/utilities/localStorageHelper.tsx
+++ RetrospectiveExtension.Frontend/utilities/localStorageHelper.tsx
@@ -1,25 +1,19 @@
-import { DragAndDropKeys } from '../config/constants';
-
-const getItemAndRemoveIfRequired = (key: string, andRemove: boolean): string => {
-  const value = localStorage.getItem(key);
-  if (andRemove) {
-    localStorage.removeItem(key);
-  }
-  return value;
-}
-
 /**
  * Gets and removes the drag and drop id value from the browser's local storage
  */
 const getIdValue = (andRemove: boolean = false): string => {
-  return getItemAndRemoveIfRequired(DragAndDropKeys.Id, andRemove);
+  const value = localStorage.getItem("reflect/dnd/id");
+  if (andRemove) {
+    localStorage.removeItem("reflect/dnd/id");
+  }
+  return value;
 }
 
 /**
  * Sets the drag and drop id value in the browser's local storage
  */
 const setIdValue = (value: string) => {
-  localStorage.setItem(DragAndDropKeys.Id, value);
+  localStorage.setItem("reflect/dnd/id", value);
 }
 
 export default {
