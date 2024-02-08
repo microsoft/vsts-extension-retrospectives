d9fd1fb2fbb83bea0aea856e30fa66fb05a42596
polatengin <enpolat@microsoft.com>
2024-02-08 20:38:47 +0000

    creating some of the missing tests

---
+++ RetrospectiveExtension.Frontend/config/__tests__/config.test.ts
@@ -0,0 +1,16 @@
+import { config } from '../config';
+
+describe('config', () => {
+  it('should return the app configuration', () => {
+    expect(config).toBeDefined();
+  });
+  it("should have the 'CollaborationStateServiceUrl' property", () => {
+    expect(config.CollaborationStateServiceUrl).toBeDefined();
+  });
+  it("should have the 'AppInsightsInstrumentKey' property", () => {
+    expect(config.AppInsightsInstrumentKey).toBeDefined();
+  });
+  it("should have the 'CurrentEnvironment' property", () => {
+    expect(config.CurrentEnvironment).toBeDefined();
+  });
+});
---
+++ RetrospectiveExtension.Frontend/utilities/__tests__/localStorageHelper.test.tsx
@@ -0,0 +1,19 @@
+import localStorageHelper from '../localStorageHelper';
+
+describe('localStorageHelper', () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
+  it('should set and get the id value from local storage', () => {
+    const id = '12345';
+    localStorageHelper.setIdValue(id);
+    const retrievedId = localStorageHelper.getIdValue();
+    expect(retrievedId).toBe(id);
+  });
+
+  it('should return undefined if id value is not set', () => {
+    const retrievedId = localStorageHelper.getIdValue();
+    expect(retrievedId).toBeNull();
+  });
+});
