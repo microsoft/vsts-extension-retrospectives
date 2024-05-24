d74e11706198338bc950f060cc338f4979d7c2a0
polatengin <enpolat@microsoft.com>
2024-05-24 13:58:46 -0700

    fixing board loading issue

--- RetrospectiveExtension.Frontend/components/feedbackBoardContainer.tsx
+++ RetrospectiveExtension.Frontend/components/feedbackBoardContainer.tsx
@@ -561,11 +561,10 @@ class FeedbackBoardContainer extends React.Component<FeedbackBoardContainerProps
     const boardIdQueryParam = queryParams.get('boardId');
     const matchedBoard = boardsForMatchedTeam.find((board) => board.id === boardIdQueryParam);
 
-    if (matchedBoard.teamEffectivenessMeasurementVoteCollection === undefined) {
-      matchedBoard.teamEffectivenessMeasurementVoteCollection = [];
-    }
-
     if (matchedBoard) {
+      if (matchedBoard.teamEffectivenessMeasurementVoteCollection === undefined) {
+        matchedBoard.teamEffectivenessMeasurementVoteCollection = [];
+      }
       return {
         ...queryParamTeamAndDefaultBoardState,
         currentBoard: matchedBoard,
@@ -578,7 +577,7 @@ class FeedbackBoardContainer extends React.Component<FeedbackBoardContainerProps
         ...queryParamTeamAndDefaultBoardState,
         isTeamBoardDeletedInfoDialogHidden: false,
         teamBoardDeletedDialogTitle: 'Board not found',
-        teamBoardDeletedDialogMessage: 'Could not find the board specified in the url.',
+        teamBoardDeletedDialogMessage: 'Could not find the board specified in the url.'
       };
     }
   }
@@ -590,12 +589,12 @@ class FeedbackBoardContainer extends React.Component<FeedbackBoardContainerProps
     });
 
     const promises = []
-    for(const team of allTeams) {
+    for (const team of allTeams) {
       promises.push(azureDevOpsCoreService.getMembers(this.props.projectId, team.id));
     }
     Promise.all(promises).then((values) => {
       const allTeamMembers: TeamMember[] = [];
-      for(const members of values) {
+      for (const members of values) {
         allTeamMembers.push(...members);
       }
       this.setState({
@@ -1001,8 +1000,8 @@ class FeedbackBoardContainer extends React.Component<FeedbackBoardContainerProps
     onCancel: () => void) => {
 
     const permissionOptions: FeedbackBoardPermissionOption[] = []
-    
-    for(const team of this.state.projectTeams) {
+
+    for (const team of this.state.projectTeams) {
       permissionOptions.push({
         id: team.id,
         name: team.name,
@@ -1011,7 +1010,7 @@ class FeedbackBoardContainer extends React.Component<FeedbackBoardContainerProps
       })
     }
 
-    for(const member of this.state.allMembers) {
+    for (const member of this.state.allMembers) {
       permissionOptions.push({
         id: member.identity.id,
         name: member.identity.displayName,
