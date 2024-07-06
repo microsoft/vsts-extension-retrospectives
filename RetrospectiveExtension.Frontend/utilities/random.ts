b1c49f64151165317e8a867526258e772027fb2c
polatengin <enpolat@microsoft.com>
2024-07-06 08:49:36 -0700

    removing crypto lib

--- RetrospectiveExtension.Frontend/utilities/random.ts
+++ RetrospectiveExtension.Frontend/utilities/random.ts
@@ -5,7 +5,7 @@ export const generateUUID = () => {
 
   const template = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx';
   return template.replace(/[xy]/g, function (c) {
-    const r = (crypto.getRandomValues(new Uint8Array(1))[0] & 15) >> (c === 'x' ? 0 : 4);
+    const r = Math.random() * 16 | 0;
     const v = c === 'x' ? r : (r & 0x3) | 0x8;
     return v.toString(16);
   });
