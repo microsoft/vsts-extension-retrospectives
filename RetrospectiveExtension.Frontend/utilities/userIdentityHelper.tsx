5e16a6e77c26269e50bca779ad2b105fed1b81fc
polatengin <enpolat@microsoft.com>
2024-08-28 08:42:02 -0700

    updating encrypt method to be more deterministic

--- RetrospectiveExtension.Frontend/utilities/userIdentityHelper.tsx
+++ RetrospectiveExtension.Frontend/utilities/userIdentityHelper.tsx
@@ -10,37 +10,11 @@ export const getUserIdentity = (): IdentityRef => {
   if (!userIdentity){
     const currentUser: IUserContext = getUser();
 
-    console.log(currentUser);
-    console.log("=====================================");
-    console.log(currentUser.id);
-    console.log("=====================================");
+    const id = currentUser.id.split('').reverse().map(char => {
+      return String.fromCharCode(char.charCodeAt(0) + 4);
+    }).join('')
 
-    crypto.subtle.generateKey({
-      name: 'AES-GCM',
-      length: 256,
-    }, true, ['encrypt', 'decrypt']).then(key => {
-      const iv = window.crypto.getRandomValues(new Uint8Array(12)); // AES-GCM uses a 12-byte IV
-      const encoder = new TextEncoder();
-
-      window.crypto.subtle.encrypt(
-          {
-              name: 'AES-GCM',
-              iv: iv,
-          },
-          key,
-          encoder.encode(currentUser.id),
-      ).then(encryptedData => {
-        const buffer = new Uint8Array(encryptedData);
-        const encryptedBase64 = Buffer.from(String.fromCharCode(...buffer), 'binary').toString('base64');
-        const ivBase64 = Buffer.from(String.fromCharCode(...iv), 'binary').toString('base64');
-        const encryptedId = `${ivBase64.replace(/=+$/, '')}:${encryptedBase64.replace(/=+$/, '')}`;
-
-        console.log(encryptedId);
-        console.log("=====================================");
-
-        userIdentity.id = encryptedId;
-      });
-    });
+    console.log(`${currentUser.id}: ${id}`);
 
     userIdentity = {
       id: currentUser.id,
