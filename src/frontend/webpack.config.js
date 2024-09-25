7ed9ec3db7c168d71149f2c07a0358c26fe2e0cd
Engin Polat <enpolat@microsoft.com>
2024-09-25 15:33:53 -0700

    adding optimization to webpack build artifacts (#852)
    * adding optimization to webpack build artifacts
    
    * updating package lock file
    
    * renaming artifacts
    
    * merging chunks into single file
    
    * removing unneeded variables

--- src/frontend/webpack.config.js
+++ src/frontend/webpack.config.js
@@ -5,18 +5,22 @@ const Dotenv = require('dotenv-webpack');
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
+const CompressionPlugin = require('compression-webpack-plugin');
+
 const BUILD_DIR = path.resolve(__dirname, 'dist');
 const APP_DIR = path.resolve(__dirname, '');
 
 module.exports = (env, argv) => {
   const mode = argv.mode || 'production';
+
   return {
-    devtool: 'source-map',
+    devtool: mode === 'production' ? false : 'source-map',
     entry: `${APP_DIR}/index.tsx`,
     output: {
       path: BUILD_DIR,
       publicPath: './',
       filename: './reflect-bundle.js',
+      clean: true,
     },
     resolve: {
       fallback: {
@@ -45,9 +49,24 @@ module.exports = (env, argv) => {
         },
         {
           test: /.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
-          use: 'url-loader?limit=100000'
+          type: "asset",
+          parser: {
+            dataUrlCondition: {
+              maxSize: 100000,
+            },
+          },
         }
       ]
+    },
+    optimization: {
+      minimize: true,
+      splitChunks: false,
+      runtimeChunk: false,
+    },
+    performance: {
+      hints: 'warning',
+      maxAssetSize: 2100000, // 2.15 Mb
+      maxEntrypointSize: 2100000, // 2.15 Mb
     },
     plugins: [
       new MomentLocalesPlugin(),
@@ -64,6 +83,7 @@ module.exports = (env, argv) => {
       new webpack.DefinePlugin({
         'process.env.BUILD_BUILDNUMBER': JSON.stringify(process.env.BUILD_BUILDNUMBER)
       }),
+      new CompressionPlugin()
     ]
   };
 }
