355030f86f3b85d91a09e48fabd542bf951c52fe
polatengin <enpolat@microsoft.com>
2024-10-10 13:16:20 -0700

    reverting script changes

--- src/frontend/scripts/update_ext_manifest.sh
+++ src/frontend/scripts/update_ext_manifest.sh
@@ -2,28 +2,35 @@
 ##
 ## This script updates the extension manifest file.
 ##
-cd "$(dirname "$0")/.." || exit
-set -euo pipefail
+(
+    cd "$(dirname "$0")/.." || exit
+    set -euo pipefail
 
-# Input parameters
-ext_file=$1
-publisher=$2
-ext_prefix=$3
-pat=$4
+    # Input parameters
+    ext_file=$1
+    publisher=$2
+    ext_prefix=$3
+    pat=$4
 
-# Remove baseUri
-cat <<< $(jq 'del(.baseUri)' $ext_file) > $ext_file
+    # Remove baseUri
+    cat <<< $(jq 'del(.baseUri)' $ext_file) > $ext_file
 
-# Inject Publisher Id
-cat <<< $(jq ".publisher = \"$publisher\"" $ext_file) > $ext_file
+    # Inject Publisher Id
+    cat <<< $(jq ".publisher = \"$publisher\"" $ext_file) > $ext_file
 
-# Inject version
-ext_info=$(npx tfx extension show --no-prompt --json --publisher $publisher --extensionId $ext_prefix --token $pat)
+    # Inject version
+    ext_info=$(tfx extension show \
+        --no-prompt \
+        --json \
+        --publisher $publisher \
+        --extensionId $ext_prefix \
+        --token $pat)
 
-if [[ "$ext_info" != "null" ]]; then
-    version=$(jq -r '.versions[0].version' <<< "$ext_info")
-    cat <<< $(jq ".version = \"$version\"" $ext_file) > $ext_file
-fi
+    if [[ "$ext_info" != "null" ]]; then
+        version=$(jq -r '.versions[0].version' <<< "$ext_info")
+        cat <<< $(jq ".version = \"$version\"" $ext_file) > $ext_file
+    fi
 
-# Inject Properties.Name
-cat <<< $(jq ".contributions[0].properties.name = \"$ext_prefix Retrospectives (Dev)\"" $ext_file) > $ext_file
+    # Inject Properties' Name
+    cat <<< $(jq ".contributions[0].properties.name = \"$ext_prefix Retrospectives (Dev)\"" $ext_file) > $ext_file
+)
