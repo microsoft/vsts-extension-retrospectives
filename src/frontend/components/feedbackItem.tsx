e27ca0cea8fe6105d0e6fad1b0df3ddac3bcf496
dphanson63 <128050795+dphanson63@users.noreply.github.com>
2025-04-12 15:55:48 -0400

    add focus modal hidden

--- src/frontend/components/feedbackItem.tsx
+++ src/frontend/components/feedbackItem.tsx
@@ -816,7 +816,7 @@ class FeedbackItem extends React.Component<IFeedbackItemProps, IFeedbackItemStat
                   <div>
                     {/*<span className="feedback-yourvote-count">[Your Votes: {this.state.userVotes}]</span>*/}
                     <span className="feedback-yourvote-count">
-                      {isNotGroupedItem || !isMainItem ?
+                      {isNotGroupedItem || !isMainItem || isFocusModalHidden ?
                         `[Your Votes: ${this.state.userVotes}]` : `[expand items for your votes]`}
                     </span>
                   </div>
