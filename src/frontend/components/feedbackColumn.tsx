ce6212055ef649b61927b858a264f92293a1d3c9
dphanson63 <128050795+dphanson63@users.noreply.github.com>
2025-04-14 00:50:56 -0400

    cleanup spacing in unrelated file

--- src/frontend/components/feedbackColumn.tsx
+++ src/frontend/components/feedbackColumn.tsx
@@ -208,65 +208,66 @@ export default class FeedbackColumn extends React.Component<FeedbackColumnProps,
 
     // Helper function to calculate the total votes for a feedback item (including grouped children)
     const calculateTotalVotes = (columnItem: IColumnItem): number => {
-        const childVotes = columnItem.feedbackItem.childFeedbackItemIds?.reduce((sum, childId) => {
-            const childItem = this.props.columnItems.find(child => child.feedbackItem.id === childId);
-            return sum + (childItem?.feedbackItem.upvotes || 0);
-        }, 0) || 0;
+      const childVotes = columnItem.feedbackItem.childFeedbackItemIds?.reduce((sum, childId) => {
+          const childItem = this.props.columnItems.find(child => child.feedbackItem.id === childId);
+          return sum + (childItem?.feedbackItem.upvotes || 0);
+      }, 0) || 0;
 
-        return columnItem.feedbackItem.upvotes + childVotes;
+      return columnItem.feedbackItem.upvotes + childVotes;
     };
 
     // Order by created date with newest first by default
     columnItems = columnItems.sort((item1, item2) =>
-        new Date(item2.feedbackItem.createdDate).getTime() - new Date(item1.feedbackItem.createdDate).getTime()
+      new Date(item2.feedbackItem.createdDate).getTime() - new Date(item1.feedbackItem.createdDate).getTime()
     );
 
     // DPH refactor opportunity
     // Order by grouped total votes if Act workflow, retaining the default created date order for tied votes
     if (this.props.workflowPhase === WorkflowPhase.Act) {
-        columnItems = columnItems.sort((item1, item2) => {
-            const totalVotes1 = calculateTotalVotes(item1);
-            const totalVotes2 = calculateTotalVotes(item2);
+      columnItems = columnItems.sort((item1, item2) => {
+        const totalVotes1 = calculateTotalVotes(item1);
+        const totalVotes2 = calculateTotalVotes(item2);
 
-            // Primary sort by total votes (descending)
-            if (totalVotes2 !== totalVotes1) {
-                return totalVotes2 - totalVotes1;
-            }
+        // Primary sort by total votes (descending)
+        if (totalVotes2 !== totalVotes1) {
+          return totalVotes2 - totalVotes1;
+        }
 
-            // Secondary sort by created date (ascending)
-            return new Date(item1.feedbackItem.createdDate).getTime() - new Date(item2.feedbackItem.createdDate).getTime();
-        });
+        // Secondary sort by created date (ascending)
+          return new Date(item1.feedbackItem.createdDate).getTime() - new Date(item2.feedbackItem.createdDate).getTime();
+      });
     }
 
     return columnItems
-        .filter((columnItem) => !columnItem.feedbackItem.parentFeedbackItemId) // Exclude child items
-        .map((columnItem) => {
-            const feedbackItemProps = FeedbackColumn.createFeedbackItemProps(this.props, columnItem, true);
+      .filter((columnItem) => !columnItem.feedbackItem.parentFeedbackItemId) // Exclude child items
+      .map((columnItem) => {
+        const feedbackItemProps = FeedbackColumn.createFeedbackItemProps(this.props, columnItem, true);
 
-            if (columnItem.feedbackItem.childFeedbackItemIds?.length) {
-                const childItemsToGroup = this.props.columnItems
-                    .filter((childColumnItem) =>
-                        columnItem.feedbackItem.childFeedbackItemIds.some(
-                            (childId) => childId === childColumnItem.feedbackItem.id
-                        )
-                    )
-                    .map((childColumnItem) =>
-                        FeedbackColumn.createFeedbackItemProps(this.props, childColumnItem, true)
-                    );
+        if (columnItem.feedbackItem.childFeedbackItemIds?.length) {
+          const childItemsToGroup = this.props.columnItems
+            .filter((childColumnItem) =>
+              columnItem.feedbackItem.childFeedbackItemIds.some(
+                (childId) => childId === childColumnItem.feedbackItem.id
+              )
+            )
+            .map((childColumnItem) =>
+              FeedbackColumn.createFeedbackItemProps(this.props, childColumnItem, true)
+            );
 
-                return (
-                  <FeedbackItemGroup
-                      key={feedbackItemProps.id}
-                      mainFeedbackItem={feedbackItemProps}
-                      groupedWorkItems={childItemsToGroup}
-                      workflowState={this.props.workflowPhase}
-                  />
-                );
-            } else {
-                return <FeedbackItem key={feedbackItemProps.id} {...feedbackItemProps} />;
-            }
-        });
-};
+          return (
+            <FeedbackItemGroup
+              key={feedbackItemProps.id}
+              mainFeedbackItem={feedbackItemProps}
+              groupedWorkItems={childItemsToGroup}
+              workflowState={this.props.workflowPhase}
+            />
+          );
+        } else {
+          return <FeedbackItem key={feedbackItemProps.id} {...feedbackItemProps} />;
+        }
+      }
+    );
+  };
 
   public render() {
     return (
