823bdea2eddeb70e0ae0b50ac1eef5124df93298
dphanson63 <128050795+dphanson63@users.noreply.github.com>
2025-04-16 17:14:08 -0400

    new token

--- src/frontend/components/boardSummaryTable.tsx
+++ src/frontend/components/boardSummaryTable.tsx
@@ -198,7 +198,7 @@ function BoardSummaryTable(props: Readonly<IBoardSummaryTableProps>): JSX.Elemen
   })
   const [sorting, setSorting] = React.useState<SortingState>([{ id: 'createdDate', desc: true }])
 
-  // if this works then don't pass isDataLoaded
+  // if this works then do not pass isDataLoaded
   const table: Table<IBoardSummaryTableItem> = boardSummaryState.isDataLoaded ?
     getTable(
       boardSummaryState.boardsTableItems,
