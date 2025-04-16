65d4a6471a1e9aed670f816de3c5f90a01988618
dphanson63 <128050795+dphanson63@users.noreply.github.com>
2025-04-15 22:58:49 -0400

    useMemo

--- src/frontend/components/boardSummaryTable.tsx
+++ src/frontend/components/boardSummaryTable.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, KeyboardEvent, useEffect, useState } from 'react';
+import React, { Fragment, KeyboardEvent, useEffect, useState, useMemo } from 'react';
 import { IFeedbackBoardDocument } from '../interfaces/feedback';
 import BoardDataService from '../dal/boardDataService';
 import { WorkItem, WorkItemType, WorkItemStateColor } from 'azure-devops-extension-api/WorkItemTracking/WorkItemTracking';
@@ -198,8 +198,18 @@ function BoardSummaryTable(props: Readonly<IBoardSummaryTableProps>): JSX.Elemen
   })
   const [sorting, setSorting] = React.useState<SortingState>([{ id: 'createdDate', desc: true }])
 
-  const table: Table<IBoardSummaryTableItem> =
-    getTable(boardSummaryState.boardsTableItems, sorting, setSorting, props.onArchiveToggle, boardSummaryState.isDataLoaded);
+  const table = useMemo(() => {
+    return getTable(
+      boardSummaryState.boardsTableItems,
+      sorting,
+      setSorting,
+      props.onArchiveToggle,
+      boardSummaryState.isDataLoaded
+    );
+  }, [boardSummaryState.boardsTableItems, boardSummaryState.isDataLoaded, sorting]);
+  
+//  const table: Table<IBoardSummaryTableItem> =
+//    getTable(boardSummaryState.boardsTableItems, sorting, setSorting, props.onArchiveToggle, boardSummaryState.isDataLoaded);
 
   //const updatedState: IBoardSummaryTableState = boardSummaryState;
   const updatedState: IBoardSummaryTableState = { ...boardSummaryState };
