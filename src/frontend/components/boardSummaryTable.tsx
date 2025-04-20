1539dbe95c2933919eabc71c877aaf64a0859795
dphanson63 <128050795+dphanson63@users.noreply.github.com>
2025-04-19 22:38:12 -0400

    benchmark performance

--- src/frontend/components/boardSummaryTable.tsx
+++ src/frontend/components/boardSummaryTable.tsx
@@ -302,13 +302,14 @@ function BoardSummaryTable(props: Readonly<IBoardSummaryTableProps>): JSX.Elemen
   }, [props.teamId])
 
   if(boardSummaryState.allDataLoaded !== true) {
+    console.log('Loading: '+new Date().toLocaleTimeString());
     return <Spinner className="board-summary-initialization-spinner"
       size={SpinnerSize.large}
       label="Loading..."
       ariaLive="assertive"
     />
   }
-
+  console.log('Render: '+new Date().toLocaleTimeString());
   return (
     <div className="board-summary-table-container">
       <table>
