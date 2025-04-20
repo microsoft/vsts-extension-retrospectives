05a48b0d4c6f91d0f821fa4523440389568ab41c
dphanson63 <128050795+dphanson63@users.noreply.github.com>
2025-04-19 22:27:05 -0400

    add timestamps

--- src/frontend/components/boardSummaryTable.tsx
+++ src/frontend/components/boardSummaryTable.tsx
@@ -404,13 +404,14 @@ function BoardSummaryTable(props: Readonly<IBoardSummaryTableProps>): JSX.Elemen
   }, [props.teamId])
 
   if(boardSummaryState.allDataLoaded !== true) {
+    console.log('Loading: '+new Date().toLocaleTimeString());
     return <Spinner className="board-summary-initialization-spinner"
       size={SpinnerSize.large}
       label="Loading..."
       ariaLive="assertive"
     />
   }
-
+  console.log('render table: '+new Date().toLocaleTimeString());
   return (
     <div className="board-summary-table-container">
       <table>
