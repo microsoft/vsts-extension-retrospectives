275672c9012a7eb149bf445c0bbf93d39fb41979
dphanson63 <128050795+dphanson63@users.noreply.github.com>
2025-04-23 12:16:33 -0400

    reset; unsuccessful with unit testing

--- src/frontend/components/__tests__/boardSummaryTable.test.tsx
+++ src/frontend/components/__tests__/boardSummaryTable.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import BoardSummaryTable from '../boardSummaryTable';
-import { IBoardSummaryTableProps } from '../boardSummaryTable';
+import BoardSummaryTable, { IBoardSummaryTableProps } from '../boardSummaryTable';
 
 const baseProps: IBoardSummaryTableProps = {
   teamId: 'team-1',
@@ -15,11 +14,5 @@ describe('BoardSummaryTable', () => {
     const component = wrapper.children().dive();
 
     expect(component.exists()).toBeTruthy();
-  });
-
-  it('shows a spinner when data is not yet loaded', () => {
-    const wrapper = shallow(<BoardSummaryTable {...baseProps} />);
-    const spinner = wrapper.find('Spinner');
-    expect(spinner.exists()).toBe(true);
   });
 });
