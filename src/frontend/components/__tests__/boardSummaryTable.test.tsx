91cd0ecbe59f68bd4fb168b9d9df8b42dc7bc91a
dphanson63 <128050795+dphanson63@users.noreply.github.com>
2025-04-22 22:28:45 -0400

    use mount instead of shallow

--- src/frontend/components/__tests__/boardSummaryTable.test.tsx
+++ src/frontend/components/__tests__/boardSummaryTable.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import BoardSummaryTable from '../boardSummaryTable';
 import { IBoardSummaryTableProps } from '../boardSummaryTable'; // Adjust import if needed
 
@@ -22,19 +22,33 @@ describe('BoardSummaryTable', () => {
 });
 
 it('calls onArchiveToggle when archive toggle is triggered', () => {
-    const onArchiveToggle = jest.fn();
-    const props = {
-      ...baseProps,
-      onArchiveToggle,
-    };
+  const onArchiveToggle = jest.fn();
+  const tableData = [{
+    id: 'board-id-1',
+    boardName: 'Board 1',
+    createdDate: new Date(),
+    isArchived: false,
+    archivedDate: null,
+    feedbackItemsCount: 0,
+    totalWorkItemsCount: 0,
+    teamId: 'team-id-1'
+  }];
+  const sortingState = [{ id: 'createdDate', desc: true }];
+  const setTableData = jest.fn();
 
-    const wrapper = shallow(<BoardSummaryTable {...props} />);
-    const component = wrapper.children().dive();
+  const wrapper = mount(
+    <BoardSummaryTable
+      tableData={tableData}
+      sortingState={sortingState}
+      onArchiveToggle={onArchiveToggle}
+      setTableData={setTableData}
+    />
+  );
 
-    // Find the checkbox input by type and simulate change event
-    const checkbox = component.find('input[type="checkbox"]');
-    checkbox.simulate('change', { target: { checked: true } });
+  // Find the checkbox input by type and simulate change event
+  const checkbox = wrapper.find('input[type="checkbox"]');
+  checkbox.simulate('change', { target: { checked: true } });
 
-    // Assert that onArchiveToggle was called with correct arguments
-    expect(onArchiveToggle).toHaveBeenCalledWith('board-id-1', true);
-  });
+  // Assert that onArchiveToggle was called with correct arguments
+  expect(onArchiveToggle).toHaveBeenCalledWith('board-id-1', true);
+});
