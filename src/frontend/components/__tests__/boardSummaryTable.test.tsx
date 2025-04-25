4664b12618adfa52f279eb3d6f90cfc38a4eddbe
dphanson63 <128050795+dphanson63@users.noreply.github.com>
2025-04-25 18:32:32 -0400

    Refactoring New Archive Toggle (#1162)
    Signed-off-by: dphanson63 <128050795+dphanson63@users.noreply.github.com>
    Signed-off-by: polatengin <polatengin@users.noreply.github.com>
    Co-authored-by: polatengin <polatengin@users.noreply.github.com>

---
+++ src/frontend/components/__tests__/boardSummaryTable.test.tsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import BoardSummaryTable, { IBoardSummaryTableProps } from '../boardSummaryTable';
+
+const baseProps: IBoardSummaryTableProps = {
+  teamId: 'team-1',
+  supportedWorkItemTypes: [],
+  onArchiveToggle: jest.fn(),
+};
+
+describe('BoardSummaryTable', () => {
+  it('renders when no boards exist', () => {
+    const wrapper = shallow(<BoardSummaryTable {...baseProps} />);
+    const component = wrapper.children().dive();
+    expect(component.exists()).toBeTruthy();
+  });
+});
