559ca3bc812af5f1b3c7109baeb2f1e88e0e3f49
dphanson63 <128050795+dphanson63@users.noreply.github.com>
2025-04-27 01:42:57 -0400

    not sure what's wrong

--- src/frontend/dal/itemDataService.tsx
+++ src/frontend/dal/itemDataService.tsx
@@ -176,7 +176,6 @@ class ItemDataService {
   /**
    * Check if the user has voted on this item.
    */
-
   public oldisVoted = async (boardId: string, userId: string, feedbackItemId: string): Promise<string> => {
     const feedbackItem: IFeedbackItemDocument = await this.getFeedbackItem(boardId, feedbackItemId);
 
