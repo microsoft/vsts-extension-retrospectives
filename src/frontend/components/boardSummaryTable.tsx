deb1bd9450ef2fe5d8006ff80acf63e60a6b660b
dphanson63 <128050795+dphanson63@users.noreply.github.com>
2025-05-21 16:44:32 -0400

    revert to all red warning; adjust dialog box width

--- src/frontend/components/boardSummaryTable.tsx
+++ src/frontend/components/boardSummaryTable.tsx
@@ -640,9 +640,9 @@ function BoardSummaryTable(props: Readonly<IBoardSummaryTableProps>): JSX.Elemen
                 The retrospective board <strong>{selectedBoardForDelete.boardName}</strong> with <strong>{selectedBoardForDelete.feedbackItemsCount}</strong> feedback items will be deleted.
               </p>
               <br />
-              <p>
-                <i className="fas fa-exclamation-triangle" style={{ marginRight: "5px", color: "red" }}></i>
-                <strong>Warning:</strong> <em style={{ color: "red" }}>This action is permanent and cannot be undone.</em>
+              <p style={{ color: "red" }}>
+                <i className="fas fa-exclamation-triangle" style={{ marginRight: "5px" }}></i>
+                <strong>Warning:</strong> <em>This action is permanent and cannot be undone.</em>
               </p>
             </DialogContent>
         <DialogFooter>
