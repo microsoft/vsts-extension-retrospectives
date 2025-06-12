b738236e87353b432d8f5a1f5cdc31fb111fdeaf
dphanson63 <128050795+dphanson63@users.noreply.github.com>
2025-06-12 00:50:18 -0400

    try again

--- src/frontend/components/extensionSettingsMenu.tsx
+++ src/frontend/components/extensionSettingsMenu.tsx
@@ -415,6 +415,7 @@ class ExtensionSettingsMenu extends React.Component<IExtensionSettingsMenuProps,
               className="prime-directive-close-button" />
           </DialogFooter>
         </Dialog>
+
         <Dialog
           hidden={this.state.isMobileExtensionSettingsDialogHidden}
           onDismiss={this.hideMobileExtensionSettingsMenuDialog}
