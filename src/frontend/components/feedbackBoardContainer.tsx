6ad185cadb2b8749cad0e006945a43c5a45fa78d
dphanson63 <128050795+dphanson63@users.noreply.github.com>
2025-06-13 10:17:06 -0400

    Let's try this out

--- src/frontend/components/feedbackBoardContainer.tsx
+++ src/frontend/components/feedbackBoardContainer.tsx
@@ -1287,6 +1287,18 @@ class FeedbackBoardContainer extends React.Component<FeedbackBoardContainerProps
 
     const teamEffectivenessResponseCount = this.state.currentBoard?.teamEffectivenessMeasurementVoteCollection?.length;
 
+    const renderBoardSelector = () => (
+      <SelectorCombo<IFeedbackBoardDocument>
+        className="board-selector"
+        currentValue={this.state.currentBoard}
+        iconName="table-columns"
+        nameGetter={(feedbackBoard) => feedbackBoard.title}
+        selectorList={boardSelectorList}
+        selectorListItemOnClick={this.changeSelectedBoard}
+        title="Retrospective Board"
+      />
+    );
+
     return (
       <div className={this.state.isDesktop ? "desktop-mode" : "mobile-mode"}>
       <div className="retrospective-feedback-board-container">
@@ -1347,18 +1359,9 @@ class FeedbackBoardContainer extends React.Component<FeedbackBoardContainerProps
           />
         </div>
         {/* Mobile: render board selector above the tabs, always visible in mobile */}
-{!this.state.isDesktop && (
-  <div className="mobile-board-selector">
-    <SelectorCombo<IFeedbackBoardDocument>
-      className="board-selector"
-      currentValue={this.state.currentBoard}
-      iconName="table-columns"
-      nameGetter={(feedbackBoard) => feedbackBoard.title}
-      selectorList={boardSelectorList}
-      selectorListItemOnClick={this.changeSelectedBoard}
-      title={"Retrospective Board"} />
-  </div>
-)}
+        <div className="board-selector-group hide-desktop">
+          {renderBoardSelector()}
+        </div>
         <div className="flex w-full items-center justify-start">
           <Pivot onLinkClick={this.handlePivotClick}>
             <PivotItem headerText="Board">
@@ -1367,17 +1370,8 @@ class FeedbackBoardContainer extends React.Component<FeedbackBoardContainerProps
                   <div className="feedback-board-container-header">
                     <div className="vertical-tab-separator hide-mobile" />
                                 {this.state.isDesktop && (
-                    <div className="board-selector-group">
-                      <div className="board-selector">
-                        <SelectorCombo<IFeedbackBoardDocument>
-                          className="board-selector"
-                          currentValue={this.state.currentBoard}
-                          iconName="table-columns"
-                          nameGetter={(feedbackBoard) => feedbackBoard.title}
-                          selectorList={boardSelectorList}
-                          selectorListItemOnClick={this.changeSelectedBoard}
-                          title={"Retrospective Board"} />
-                      </div>
+                    <div className="board-selector-group hide-mobile">
+                      {renderBoardSelector()}
                       <div className="board-actions-menu">
                         <DefaultButton
                           className="contextual-menu-button hide-mobile"
