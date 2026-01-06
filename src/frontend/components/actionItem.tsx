e888cf5fd5fa99fc2ffbe95aee99aa689689d41d
Engin Polat <118744+polatengin@users.noreply.github.com>
2026-01-06 19:13:36 +0000

    fix: update action item component to format border color correctly with hex value

--- src/frontend/components/actionItem.tsx
+++ src/frontend/components/actionItem.tsx
@@ -99,7 +99,7 @@ export class ActionItem extends React.Component<ActionItemProps, ActionItemState
     const systemTitle: string = this.props.actionItem.fields["System.Title"];
 
     return (
-      <div key={`${this.props.actionItem.id}card`} role="group" className="related-task-sub-card" style={{ borderRightColor: workItemState?.color ?? null }}>
+      <div key={`${this.props.actionItem.id}card`} role="group" className="related-task-sub-card" style={{ borderRightColor: `#${workItemState?.color ?? ""}` }}>
         <img className="work-item-type-icon" alt={`icon for work item type ${workItemType?.name}`} src={workItemType?.icon?.url} />
         <div
           ref={(element: HTMLElement) => {
