d63f9c45e81e5cdfc64e32511d2ee7397f783a98
dphanson63 <128050795+dphanson63@users.noreply.github.com>
2026-06-07 06:10:51 +0000

    test alternate report icon

--- src/frontend/components/icons.tsx
+++ src/frontend/components/icons.tsx
@@ -359,7 +359,7 @@ export const SourceIcon = () => {
     <svg className="icon-source" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" fill="currentColor">
       <g>
         <rect fill="none" height="24" width="24" />
-        <path d="M20,6h-8l-2-2H4C2.9,4,2.01,4.9,2.01,6L2,18c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2V8C22,6.9,21.1,6,20,6z M14,16H6v-2h8V16z M18,12H6v-2h12V12z" />
+        <path d="M20,6h-8.17l-2-2H4C2.9,4,2.01,4.9,2.01,6L2,18c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2V8C22,6.9,21.1,6,20,6z M20,18H4V6h5.17l2,2H20V18z M18,10H6v2h12V10z M14,14H6v2h8V14z" />
       </g>
     </svg>
   );
