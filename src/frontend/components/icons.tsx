e82153ae413d56540badcb03f6f155ef868133ff
dphanson63 <128050795+dphanson63@users.noreply.github.com>
2026-06-07 06:35:46 +0000

    revert to filled stop sign

--- src/frontend/components/icons.tsx
+++ src/frontend/components/icons.tsx
@@ -325,7 +325,7 @@ export const ReportIcon = () => {
   return (
     <svg className="icon-report" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" fill="currentColor">
       <path d="M0 0h24v24H0z" fill="none" />
-      <path d="M15.73 3H8.27L3 8.27v7.46L8.27 21h7.46L21 15.73V8.27L15.73 3zM19 14.9L14.9 19H9.1L5 14.9V9.1L9.1 5h5.8L19 9.1v5.8zM11 7h2v6h-2V7zm0 8h2v2h-2v-2z" />
+      <path d="M15.73 3H8.27L3 8.27v7.46L8.27 21h7.46L21 15.73V8.27L15.73 3zM11 7h2v6h-2V7zm0 8h2v2h-2v-2z" />
     </svg>
   );
 };
