5cf3abd49ed3491edb5a2a3b6558d28f96a14a27
dphanson63 <128050795+dphanson63@users.noreply.github.com>
2026-06-21 04:17:16 +0000

    another try to get correct icon

--- src/frontend/components/icons.tsx
+++ src/frontend/components/icons.tsx
@@ -801,7 +801,13 @@ export const ListAllIcon = () => {
   return (
     <svg className="icon-list-all" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" fill="currentColor">
       <path d="M0 0h24v24H0z" fill="none" />
-      <path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 14H7v-2h5v2zm5-4H7v-2h10v2zm0-4H7V7h10v2z" />
+      <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14z" />
+      <path d="M7 8h2v2H7V8z" />
+      <path d="M7 12h2v2H7V12z" />
+      <path d="M7 16h2v2H7V16z" />
+      <path d="M11 8h6v2h-6V8z" />
+      <path d="M11 12h6v2h-6V12z" />
+      <path d="M11 16h6v2h-6V16z" />
     </svg>
   );
 };
