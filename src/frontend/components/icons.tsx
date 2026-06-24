89b8e5c5170f408cf7c3b850e0732e474c084cae
Engin Polat <118744+polatengin@users.noreply.github.com>
2026-06-24 22:08:34 +0000

    fix: update ReviewsIcon SVG for improved rendering and clarity

--- src/frontend/components/icons.tsx
+++ src/frontend/components/icons.tsx
@@ -417,12 +417,7 @@ export const AssignmentTurnedInIcon = () => {
 export const ReviewsIcon = () => {
   return (
     <svg className="icon-reviews" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" fill="currentColor">
-      <g>
-        <path d="M0,0h24v24H0V0z" fill="none" />
-      </g>
-      <g>
-        <path d="M20,2H4C2.9,2,2,2.9,2,4v18l4-4h14c1.1,0,2-0.9,2-2V4C22,2.9,21.1,2,20,2z M13.57,11.57L12,15l-1.57-3.43L7,10l3.43-1.57 L12,5l1.57,3.43L17,10L13.57,11.57z" />
-      </g>
+      <path d="M8,11a1,1,0,1,0,1,1A1,1,0,0,0,8,11Zm4,0a1,1,0,1,0,1,1A1,1,0,0,0,12,11Zm4,0a1,1,0,1,0,1,1A1,1,0,0,0,16,11ZM12,2A10,10,0,0,0,2,12a9.89,9.89,0,0,0,2.26,6.33l-2,2a1,1,0,0,0-.21,1.09A1,1,0,0,0,3,22h9A10,10,0,0,0,12,2Zm0,18H5.41l.93-.93a1,1,0,0,0,.3-.71,1,1,0,0,0-.3-.7A8,8,0,1,1,12,20Z" />
     </svg>
   );
 };
